d846a899125cdb16102fe14d0919f4f861f37fcb
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-11-28 18:02:52 -0500

    courses: inserting an array of courses and displaying it

--- courses/src/app/courses/courses.component.ts
+++ courses/src/app/courses/courses.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 
 export class CoursesComponent {
     title = "List of Courses";
+    courses = ["course1", "course2", "course3"]
 
     getTitle() {
         return this.title;
