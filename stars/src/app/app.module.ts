abf9f8637991d00934cda2328810d2cc33eb6f2d
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-11-29 09:24:42 -0500

    assignment-2: simplest solution with bootstrap 3 and glyphicon classes

--- stars/src/app/app.module.ts
+++ stars/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StarsComponent } from './stars/stars.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StarsComponent
+    StarsComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule
