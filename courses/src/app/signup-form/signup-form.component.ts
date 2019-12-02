f48f155ecb2839a64616e60ae6ba3c6eec15d404
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-02 18:19:13 -0500

    courses: custom validator

--- courses/src/app/signup-form/signup-form.component.ts
+++ courses/src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators, customMinLength } from './username.validators';
 
 @Component({
   selector: 'signup-form',
@@ -10,7 +11,9 @@ export class SignupFormComponent {
   form = new FormGroup({
     username: new FormControl('', [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace,
+      customMinLength(15)
     ]),
     password: new FormControl('', Validators.required)
   });
---
+++ courses/src/app/signup-form/username.validators.ts
@@ -0,0 +1,51 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class UsernameValidators {
+    static cannotHasX(control: AbstractControl): ValidationErrors | null {
+        if((control.value as string).indexOf('x') != -1) {
+            return {
+                cannotHasX: 'aaaa'
+            };
+        }
+
+        return null;
+    }
+    static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+        if((control.value as string).indexOf(' ') != -1) {
+            return {
+                cannotContainSpace: true
+            };
+        }
+
+        return null;
+    }
+    static customMinLength(control: AbstractControl, minLengthValue: number = 4): ValidationErrors | null {
+        if((control.value as string).length < minLengthValue) {
+            return {
+                customMinLength: {
+                    requiredLength: minLengthValue,
+                    actualLength: control.value.length
+                }
+            };
+        }
+
+        return null;
+    }
+}
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+        const forbidden = nameRe.test(control.value);
+        return forbidden ? {'forbiddenName': {value: control.value}} : null;
+    };
+}
+export function customMinLength(minLen: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const hasMin = (control.value as string).length < minLen;
+        return hasMin ? {
+            aliasMinLength: {
+                value: control.value, 
+                requiredLength: minLen
+            }
+        } : null;
+    };
+}
