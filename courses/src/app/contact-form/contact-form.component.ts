b80f51082f9d580c2b192fa9e47cae7fe6e6f48e
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-02 11:20:13 -0500

    courses: validation : basic validation

--- courses/src/app/contact-form/contact-form.component.ts
+++ courses/src/app/contact-form/contact-form.component.ts
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  // firstName: string;
+  comment: string;
 
   constructor() { }
+
+  onSubmit() {
+    // console.log(`[onSubmit()]\n\tfirstName: ${ this.firstName }\n\tcomment: ${ this.comment }`);
+  }
+
+  log(x) {
+    console.log(`[log()]\n\tx: %o`, x);
+  }
 
   ngOnInit() {
   }
