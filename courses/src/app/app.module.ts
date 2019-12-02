e2a47e5461d888cdd53c781b73e9a8e7cb353eeb
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-02 17:10:05 -0500

    courses : add signup-form

--- courses/src/app/app.module.ts
+++ courses/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
-import { EmailService } from './email.service';
+import { EmailService } from './services/email.service';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { CourseService } from './course/course.service';
 import { LikeComponent } from './like/like.component';
@@ -12,6 +12,7 @@ import { ZippyComponent } from './zippy/zippy.component';
 import { InputFormatDirective } from './input-format/input-format.directive';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CourseFormComponent } from './course-form/course-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CourseFormComponent } from './course-form/course-form.component';
     ZippyComponent,
     InputFormatDirective,
     ContactFormComponent,
-    CourseFormComponent
+    CourseFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
