040062d5c7cee89ea7350870836d79f7e00647da
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-02 20:10:33 -0500

    courses: FormBuilder

--- courses/src/app/new-course-form/new-course-form.component.ts
+++ courses/src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'new-course-form',
@@ -8,17 +9,36 @@ import { FormGroup, FormArray, FormControl } from '@angular/forms';
 })
 export class NewCourseFormComponent {
   form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    contact: new FormGroup({
+      email: new FormControl(),
+      phone: new FormControl()
+    }),
     topics: new FormArray([])
   });
 
-  addTopic(topic: HTMLInputElement) {
-    this.topics.push(new FormControl(topic.value));
-    topic.value = '';
+  // OR
+
+  form2;
+  constructor(fb: FormBuilder) {
+    this.form2 = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
   }
-  removeTopic(topic: FormControl) {
-    let index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
-  }
+
+  // addTopic(topic: HTMLInputElement) {
+  //   this.topics.push(new FormControl(topic.value));
+  //   topic.value = '';
+  // }
+  // removeTopic(topic: FormControl) {
+  //   let index = this.topics.controls.indexOf(topic);
+  //   this.topics.removeAt(index);
+  // }
 
   get topics() {
     return this.form.get('topics') as FormArray;
